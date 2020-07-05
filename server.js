d123d9be7bf13984eee5c2cd7d5b0197ee1f6513
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2020-07-04 21:44:26 -0500

    adding middleware

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const graphqlHTTP = require('express-graphql')
 const schema = require('./schema').default
 const root = require('./root').default
 
+const loggingMiddleware = (req, res, next) => {
+  console.log(`New request made from: ${req.hostname}`);
+  next();
+}
+
 const app = express();
+app.use(loggingMiddleware);
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
